45ab0f0c693640461a60cd02a90f0734c970c3ba
Ayush Singh <ayush.singh@niser.ac.in>
2024-06-21 16:50:57 +0200

    nuke email newsletter

--- assets/scripts/subscribe.js
+++
@@ -1,8 +0,0 @@
-const successMessage = document.querySelector('p#success');
-const newsletterForm = document.querySelector('form#newsletter');
-
-function onSubscribe(timeout) {
-	newsletterForm.style['display'] = 'none';
-	successMessage.style['display'] = 'unset';
-	setTimeout(function() {location = '/';}, timeout);
-}
